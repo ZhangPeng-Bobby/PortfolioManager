a75662474a51fdaba65a556caabd0fcc37035303
Bobby <1054329489@qq.com>
2020-09-15 21:34:29 +0800

    new feat: netVal

--- src/main/java/com/citi/group12/dao/CashDao.java
+++ src/main/java/com/citi/group12/dao/CashDao.java
@@ -2,6 +2,7 @@ package com.citi.group12.dao;
 
 import com.citi.group12.entity.Cash;
 
+import java.util.Date;
 import java.util.List;
 
 public interface CashDao {
@@ -9,5 +10,7 @@ public interface CashDao {
     long update(Cash cash);
     List<Cash> findAll();
     Cash findOne(String id);
+    List<Cash> findByDateInterval(Date startDate, Date endDate);
     void delete(String id);
+
 }
